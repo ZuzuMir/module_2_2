e77c31b97dd3d2729845d4079d057a04e4204146
ZuzuMir <laraminnullina0247@gmail.com>
2024-10-24 11:43:39 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input('first: ')
+second = input('second: ')
+third = input('third: ')
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
